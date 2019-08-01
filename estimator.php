20dbee30479fe430237e43a1b7f58150b3fb2d7c
root <root@localhost.localdomain>
2019-08-01 13:36:43 -0400

    Initial Project Dollar Push

---
+++ estimator.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <!-- Begin Head -->
+    <head>
+        <!-- Basic -->
+        <meta charset="UTF-8">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="theme-color" content="#1890C2">
+        
+        <title>BKreative Media Solutions | Brand & Online Marketing</title> 
+        <meta name="keywords" content="bkreative, media, brand, marketing, print, logo design, web design, web development, responsive design, columbia, sc" />
+		<meta name="description" content="BKreative Media Solutions is your #1 branding & marketing solutions company providing easy, affordable services that give our clients peace of mind & assurance from start to finish." />
+		<meta name="google-site-verification" content="0uXfkBkaQ1ju3RLebflGpm-dK_dKfZ8q8Mbreu5NmAU" />
+		<meta property="twitter:account_id" content="4503599629060458" />
+		<meta name="p:domain_verify" content="517452c03835ce7f00fb9f1b4f23b31e"/>
+        
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.bkreative.net/" />
+        <meta property="og:image" content="http://www.bkreative.net/images/bkms_fblogo.png" />
+        <meta property="og:title" content="BKreative Media Solutions | Brand & Online Marketing" />
+        <meta property="og:description" content="BKreative Media Solutions is your #1 branding & marketing solutions company providing easy, affordable services that give our clients peace of mind & assurance from start to finish." />
+        <meta property="og:site_name" content="BKreative Media Solutions | Brand & Online Marketing"/>
+        
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="BKreative Media Solutions | Brand & Online Marketing">
+        <meta name="twitter:description" content="BKreative Media Solutions is your #1 branding and marketing solutions company. Our goal is to provide easy and affordable services that give our clients peace of mind and assurance from start to finish.">
+        <meta name="twitter:url" content="http://www.bkreative.net">
+        <meta name="twitter:site" content="@BKreativeMedia">
+        <meta name="twitter:creator" content="BKreativeMedia">
+        <meta name="twitter:image" content="http://www.bkreative.net/images/bkms_fblogo.png">
+        
+        <link href="style/images/bkms_applelogo.png" rel="apple-touch-icon" />
+        <link href="style/images/bkms_applelogo.png" rel="apple-touch-icon" sizes="76x76" />
+        <link href="style/images/bkms_applelogo.png" rel="apple-touch-icon" sizes="120x120" />
+        <link href="style/images/bkms_applelogo.png" rel="apple-touch-icon" sizes="152x152" />
+        <link href="style/images/bkms_applelogo.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="style/images/bkms_applelogo.png" rel="icon" sizes="192x192" />
+        <link href="style/images/bkms_applelogo.png" rel="icon" sizes="128x128" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+        <!-- Web Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <link href="css/animate.css" rel="stylesheet" type="text/css"/>
+
+
+	<?php include_once("dynamic/facebooktracking.php"); ?>
+	<?php include_once("dynamic/fbmessengertracking.php"); ?>
+	<?php include_once("dynamic/analyticstracking.php"); ?>
+	<?php //include_once("dynamic/hubspottracking.php"); ?>
+   </head>
+    <!-- End Head -->
+
+    <!-- Body -->
+    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+        <!-- HEADER -->
+        <header id="header" class="affix-top" data-spy="affix" data-offset-top="5">
+            <nav class="navbar navbar-default navbar-transparent navbar-fixed-top">
+                <div class="container">
+                    <div class="header_main">
+                        <div class="logo">
+                            <a href="#index"><img src="images/logo.jpg" class="img-responsive" alt="BKreative Media Solutions Header Logo"/></a>
+                        </div>
+                        <div class="header_right">
+                            <div class="call_top"><a href="tel:8035308110"><span>803.530.8110</span></a></div>
+                            <div class="nav">
+                                <div class="nav_main">
+                                    <div class="nav_bg">
+                                        <div class="in_bg"></div> 
+                                    </div>
+                                    <ul class="navbar-nav navbar-right">
+                                        <!--li class="has-sub active">
+                                            <a class="page-scroll" href="."><< BACK TO MAIN PAGE</a>
+                                        </li-->
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="res_navigation">
+                                <a href="javascript:void(0)" class="nav-toggle" id="trigger-overlay">
+                                    <span class="top"></span>
+                                    <span class="middle"></span>
+                                    <span class="bottom"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav> 
+        </header>    
+        <!-- END HEADER -->
+        <!-- PAGE CONTENT -->
+        <section>
+            <div class="full-height-bg" id="estimator">
+                <div class="container title">
+                    <h2>BRANDING ESTIMATE <span class="pink">CALCULATOR</span></h2>
+                    <span class="desc">The Branding Estimate Calculator was created to help potential clients understand their <strong>digital branding cost.</strong></span>
+                </div>
+                <div class="container options">
+                    <p id="question" class="question">Q1. What type of branding services do you need?</p>
+                    <div id="first_option" class="option_con">
+                        <p>Answer Choice #1</p>
+                    </div>
+                    <div id="second_option" class="option_con">
+                        <p>Answer Choice #2</p>
+                    </div>
+                    <div id="three_option" class="option_con">
+                        <p>Answer Choice #3</p>
+                    </div>
+                    <div  id="fourth_option" class="option_con" style="visibility: hidden;">
+                        <p>Testing #4</p>
+                    </div>
+                </div>
+            </div> 
+        </section>
+        <!-- END PAGE CONTENT -->
+        <!-- FOOTER -->
+        <footer class="FooterWrapper">
+            <!--div class="FooterMenu">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-xs-12">
+                            <ul>
+                                <li><a href="#index">Home</a></li>
+                                <li><a href="#about">ABOUT</a></li>
+                                <li><a href="#pricing">PRICING</a></li>
+                                <li><a href="#portfolio">PORTFOLIO</a></li>
+                                <li><a href="#contact">CONTACT</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="FooterContact">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12 column">
+                            <p>P.O. Box 292322,<br>Columbia, SC 29229</p>
+                            <a href="https://goo.gl/maps/2WUZiWQwVdx" target="_blank" class="Link">View Map</a>
+                        </div>
+                        <div class="col-sm-4 col-xs-12 column">
+                            <div class="Call">
+                                <a href="tel:8035308110">803.530.8110</a> <span>/</span> <a href="tel:8037675888">803.767.5888</a>
+                            </div>
+                            <div class="Email">
+                                <a href="mailto:info@bkreative.net">info@bkreative.net</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-xs-12 column">
+                            <ul class="Social">
+                                <li><a target="_blank" href="https://www.facebook.com/BKreativeMediaLLC">   <img alt="#" src="images/facebook.svg"></a></li>
+                                <li><a target="_blank" href="https://www.twitter.com/bkreativemedia"><img alt="#" src="images/twitter.svg"></a></li> 
+                                <li><a target="_blank" href="https://www.instagram.com/bkreativemedia/"><img alt="#" src="images/instagram.svg"></a></li> 
+                                <li><a target="_blank" href="https://www.linkedin.com/company/bkreative-media-solutions"><img alt="#" src="images/linkedin.svg"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div-->
+            <div class="CopyRightWrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-xs-12">
+                            <div class="Copy"><img alt='BKreative Media Solutions Copyright Logo' src="images/copy-right-logo.png">© 2010-19 <span>BKreative Media Solutions, LLC</span>. All Rights Reserved.</div>
+                            <a href="documents/bkreativeprivacy.pdf" target="_blank" class="right">Privacy Policy</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="javascript:void(0);" class="s-back-to-top js__back-to-top">
+                <img alt='Image of Back to Top Arrow' src="images/Up_arrow.svg"/>
+            </a>
+        </footer>
+        <!-- END FOOTER -->
+        
+        <!-- Theme Styles -->
+        <link href="css/fonts.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/responsive.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/hack.css" rel="stylesheet" type="text/css"/>
+        <link href="css/estimator.css" rel="stylesheet" type="text/css"/>
+        <!-- Favicon -->        
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>       
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("#contact_form").validate({
+
+                });
+            });
+
+            $("#form_submit").on('click', function (event) {
+                if ($('#contact_form').valid()) {
+                    event.preventDefault();
+                    var name = $("#name").val();
+                    var email = $("#email").val();
+                    var phone = $("#phone").val();
+                    var company = $("#company").val();
+                    var service_need = $("#service_need").val();
+                    var budget = $("#budget").val();
+                    var message = $("#message").val();
+
+                    $.post("save.php", {
+                        name: name,
+                        email: email,
+                        phone: phone,
+                        company: company,
+                        service_need: service_need,
+                        budget: budget,
+                        message: message
+                    },
+                        function (result) {
+                            $(".message").html("<p>Your contact information was received!<br>Please allow us 24-48 hours to follow up with you. Thanks!</p> ");
+                            $("#contact_form").trigger('reset');
+
+                        });
+                }
+            });
+        </script>
+        <script src="js/jquery.back-to-top.min.js"></script>
+        <script src="js/slick.js"></script> 
+        <script src="js/main.js"></script> 
+        <script src="js/wow.js"></script>
+        <script>
+            new WOW().init();
+        </script>
+        
+    </body>
+    <!-- End Body -->
+</html>
---
+++ js/webestimate.js
@@ -0,0 +1,23 @@
+/*  
+Title:      BKreative Website Estimator
+Author:     Ken Elliott (@kennethspeaks)
+For:        BKreative Media Solutions
+Website:    https://www.bkreative.net
+Version:    1.0.0
+*/
+
+//Call the Variables
+var price = 0;
+
+//Call the Elements
+var question1 = document.getElementById('selection1');
+var question2 = document.getElementById('selection2');
+var question3 = document.getElementById('selection3');
+var question4 = document.getElementById('selection4');
+var question5 = document.getElementById('selection5');
+var question6 = document.getElementById('selection6');
+var question7 = document.getElementById('selection7');
+var question8 = document.getElementById('selection8');
+var question9 = document.getElementById('selection9');
+
+//Begin Estimator Questionnaire
